2475bc2602e0d4132d93e98ad7afff393d04b22f
Neeraj <Neeraj@github.com>
2021-10-09 18:33:53 +0530

    Creating entities using Spring Data Cassandra.

--- src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -2,21 +2,43 @@ package com.majka.betterreadsdataloader;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import com.majka.betterreadsdataloader.author.AuthorRepository;
+import com.majka.betterreadsdataloader.connector.DataStaxAstraProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import connection.DataStaxAstraProperties;
-
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
+	@Autowired AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
+
+
+	@PostConstruct
+	public void start(){
+		System.out.println("Application started");
+	}
+
+
+
+
+
+
+
+
+
+
 
 	/**
 	 * This is necessary to have the Spring Boot app use the Astra secure bundle to
--- src/main/java/com/majka/betterreadsdataloader/connector/DataStaxAstraProperties.java
+++ src/main/java/com/majka/betterreadsdataloader/connector/DataStaxAstraProperties.java
@@ -1,4 +1,4 @@
-package connection;
+package com.majka.betterreadsdataloader.connector;
 
 import java.io.File;
 
