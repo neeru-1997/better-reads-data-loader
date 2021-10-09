b46ee26e581db32c822975e3a6d5a49bf10332dc
Neeraj <Neeraj@github.com>
2021-10-09 22:59:47 +0530

    Added Authors to Cassandra DB

--- src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,13 +1,21 @@
 package com.majka.betterreadsdataloader;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import com.majka.betterreadsdataloader.author.Author;
 import com.majka.betterreadsdataloader.author.AuthorRepository;
 import com.majka.betterreadsdataloader.connector.DataStaxAstraProperties;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -18,27 +26,55 @@ import org.springframework.context.annotation.Bean;
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
-	@Autowired AuthorRepository authorRepository;
+	@Autowired
+	AuthorRepository authorRepository;
+
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
 
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpLocation);
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				// Read and parse the line
+				String jsonString = line.substring(line.indexOf("{"));
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
 
-	@PostConstruct
-	public void start(){
-		System.out.println("Application started");
+					// Construct author object
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+					// Persist usign Repository
+					authorRepository.save(author);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private void initWorks() {
 
+	}
 
-
-
-
-
-
-
-
+	@PostConstruct
+	public void start() {
+		initAuthors();
+		initWorks();
+	}
 
 	/**
 	 * This is necessary to have the Spring Boot app use the Astra secure bundle to
