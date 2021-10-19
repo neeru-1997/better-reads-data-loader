f44b33d08fefc59ac72c3e6c4c73180c709e21af
Neeraj <Neeraj@github.com>
2021-10-19 21:38:42 +0530

    Adding Book details to Cassandra DB

--- src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/majka/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -4,14 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
 import com.majka.betterreadsdataloader.author.Author;
 import com.majka.betterreadsdataloader.author.AuthorRepository;
+import com.majka.betterreadsdataloader.book.Book;
+import com.majka.betterreadsdataloader.book.BookRepository;
 import com.majka.betterreadsdataloader.connector.DataStaxAstraProperties;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +37,9 @@ public class BetterReadsDataLoaderApplication {
 
 	@Autowired
 	AuthorRepository authorRepository;
+
+	@Autowired
+	BookRepository bookRepository;
 
 	@Value("${datadump.location.author}")
 	private String authorDumpLocation;
@@ -54,7 +66,7 @@ public class BetterReadsDataLoaderApplication {
 					author.setPersonalName(jsonObject.optString("personal_name"));
 					author.setId(jsonObject.optString("key").replace("/authors/", ""));
 
-					// Persist usign Repository
+					// Persist using Repository
 					authorRepository.save(author);
 				} catch (JSONException e) {
 					e.printStackTrace();
@@ -67,12 +79,74 @@ public class BetterReadsDataLoaderApplication {
 	}
 
 	private void initWorks() {
+		Path path = Paths.get(worksDumpLocation);
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				// Read and parse the line
+				String jsonString = line.substring(line.indexOf("{"));
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
 
+					// Construct book object
+					Book book = new Book();
+					book.setId(jsonObject.getString("key").replace("/works/", ""));
+					book.setName(jsonObject.optString("title"));
+
+					JSONObject descriptionObj = jsonObject.optJSONObject("description");
+					if (descriptionObj != null) {
+						book.setDescription(descriptionObj.optString("value"));
+					}
+
+					JSONObject publishedObj = jsonObject.optJSONObject("created");
+					if (publishedObj != null) {
+						String dateStr = publishedObj.getString("value");
+						book.setPublishedDate(LocalDate.parse(dateStr, dateFormat));
+					}
+
+					JSONArray coversJSONArr = jsonObject.optJSONArray("covers");
+					if (coversJSONArr != null) {
+						List<String> coverIds = new ArrayList<>();
+						for (int i = 0; i < coversJSONArr.length(); i++) {
+							coverIds.add(coversJSONArr.getString(i));
+						}
+						book.setCoverIds(coverIds);
+					}
+
+					JSONArray auhtorsJSONArr = jsonObject.optJSONArray("authors");
+					if (auhtorsJSONArr != null) {
+						List<String> authorIds = new ArrayList<>();
+						for (int i = 0; i < auhtorsJSONArr.length(); i++) {
+							String authorId = auhtorsJSONArr.getJSONObject(i).getJSONObject("author").getString("key")
+									.replace("/authors/", "");
+							authorIds.add(authorId);
+						}
+
+						book.setAuthorIds(authorIds);
+
+						List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+								.map(optionalAuthor -> {
+									if (!optionalAuthor.isPresent())
+										return "Unknown Author";
+									return optionalAuthor.get().getName();
+								}).collect(Collectors.toList());
+
+						book.setAuthorNames(authorNames);
+
+						bookRepository.save(book);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@PostConstruct
 	public void start() {
-		initAuthors();
+		// initAuthors();
 		initWorks();
 	}
 
